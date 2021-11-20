41648eff58e3742263faea981423fbfae63f4ddf
a100659 <a100659@alunos.uminho.pt>
2021-11-20 17:49:09 +0000

    Adcição das funções dos pontos 1 e 3

--- src/Tarefa1_2021li1g063.hs
+++ src/Tarefa1_2021li1g063.hs
@@ -8,8 +8,51 @@ Módulo para a realização da Tarefa 1 do projeto de LI1 em 2021/22.
 -}
 module Tarefa1_2021li1g063 where
 
-import LI12122
+type Coordenadas = (Int,Int) 
+data Peca = Bloco | Porta | Caixa | Vazio 
+    deriving (Show, Eq) 
+type Mapa = [[Peca]] 
+
+data Direcao = Este | Oeste 
+data Jogador = Jogador Coordenadas Direcao Bool 
+
+data Jogo = Jogo Mapa Jogador 
 
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa pecas = undefined
+validaPotencialMapa [] = False 
+validaPotencialMapa (h:t) 
+        | validaposicoes (h:t) == True && vereficarcaixas (h:t) == True = True 
+        | otherwise = False
+
+
+
+
+naorepetirposicao:: (Peca,Coordenadas) -> [(Peca,Coordenadas)] -> Bool                  -- a função vai vereficar se as coordenadas de uma peça se repetem em outra peça 
+naorepetirposicao _ [] = True  
+naorepetirposicao (p1,(x1,y1)) ((p2,(x2,y2)):t)
+        | x1 == x2 && y1==y2 = False 
+        | otherwise = naorepetirposicao (p1,(x1,y1)) t 
+ 
+
+validaposicoes:: [(Peca,Coordenadas)] -> Bool  
+validaposicoes [] = True 
+validaposicoes (h:t) 
+        | naorepetirposicao h t  = validaposicoes t 
+        | otherwise = False 
+
+
+
+
+vereficarcaixas::[(Peca,Coordenadas)] -> Bool 
+vereficarcaixas [] = True
+vereficarcaixas ((p,(x,y)):t)
+           |p == Caixa    = if (vereficardebaixo (p,(x,y)) ((p,(x,y)):t)) == True then vereficarcaixas t else False     --vereficarcaixas t       --so esta a vereficar o primeiro caso 
+           |otherwise     = vereficarcaixas t 
+
+vereficardebaixo:: (Peca,Coordenadas) -> [(Peca,Coordenadas)] -> Bool              
+vereficardebaixo _ [] = False
+vereficardebaixo (p1,(x1,y1)) ((p2,(x2,y2)):t) 
+        | p2 == Caixa && x2 == x1 && y2 == y1 + 1 = True 
+        | p2 == Bloco && x2 == x1 && y2 == y1 + 1 = True  
+        | otherwise = vereficardebaixo (p1,(x1,y1)) t 
 
