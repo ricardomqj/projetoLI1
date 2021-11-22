dff05edad356d427765baa1c6bb14b40d58e3330
Ricardo Jesus <a100066@alunos.uminho.pt>
2021-11-22 11:49:43 +0000

    Ponto 5 da Tarefa 1(incompleta)

--- src/Tarefa1_2021li1g063.hs
+++ src/Tarefa1_2021li1g063.hs
@@ -45,16 +45,31 @@ soUmaPorta :: [(Peca, Coordenadas)] -> Bool
 soUmaPorta [] = False 
 soUmaPorta l = if numPortas l == 1 then True else False 
    
+-- 5 
 
+-- função principal 
 
+-- calcula a peça que tenha o y maior, ou  seja, que seja uma das peças da base 
+yMaior :: [(Peca, Coordenadas)] -> Int 
+yMaior [] = 0 
+yMaior [(p, (x,y))] = y 
+yMaior ((p1,(x1,y1)):(p2,(x2,y2)):t)
+    | y2 > y1 && x2 > x1 = yMaior (peca1:t)
+    | otherwise = yMaior (peca2:t)
+    where peca1 = (p1, (x1,y1))
+          peca2 = (p2, (x2,y2))
 
- 
+-- devolve uma lista apenas com os blocos da base
+listaBlocosBase :: [(Peca, Coordenadas)] -> [(Peca, Coordenadas)] 
+listaBlocosBase [] = []
+listaBlocosBase ((p,(x,y)):t) 
+    | y == yMaior ((p,(x,y)):t) = peca1' : listaBlocosBase t  
+    | otherwise = listaBlocosBase t 
+    where peca1' = (p,(x,y))
 
+-- verifica se exitem espaços vazios entre os blocos da base 
 
-
-
---fazer uma função que conte o numero de portas e depois se for =1 = True 
-
-
--- ver elemMSet 
-
+verificaEspVaziosBase :: [(Peca, Coordenadas)] -> Bool 
+verificaEspVaziosBase [] = False 
+verificaEspVaziosBase ((p1,(x1,y1)):(p2,(x2,y2)):t)  
+    
