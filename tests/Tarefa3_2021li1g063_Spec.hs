de68f33a8d685577c9ad847accfc3c06755c7d4d
Ricardo Jesus <a100066@alunos.uminho.pt>
2021-11-28 23:56:13 +0000

    Alterações Testes

--- tests/Tarefa3_2021li1g063_Spec.hs
+++ tests/Tarefa3_2021li1g063_Spec.hs
@@ -8,4 +8,6 @@ testsT3 =
   test
     [ "Tarefa 3 - Teste Imprime Jogo m1e1" ~: "      <\n      X\n      X\nP   C X\nXXXXXXX" ~=?  show m1e1
     , "Tarefa 3 - Teste Imprime Jogo m1e2" ~: "       \n      X\n      X\nP < C X\nXXXXXXX" ~=?  show m1e2
-    ]
+    , "Tarefa 3 - Teste Imprime Jogo jogadorCaixa" ~: "      X\n   C  X\nP  >C X\nXXXXXXX" ~=?  show jogadorCaixa
+    ]       
+
