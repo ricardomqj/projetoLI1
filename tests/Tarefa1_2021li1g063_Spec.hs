5d1942e7090541b45035925a9700bc0a77266de3
Rui Pinto <a100659@alunos.uminho.pt>
2021-11-28 22:08:23 +0000

    Update Tarefa1_2021li1g063_Spec.hs

--- tests/Tarefa1_2021li1g063_Spec.hs
+++ tests/Tarefa1_2021li1g063_Spec.hs
@@ -11,4 +11,11 @@ testsT1 =
     [ "Tarefa 1 - Teste Valida Mapa m1r" ~: validaPotencialMapa m1 ~=? True
     , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
     , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
+    , "Tarefa 1 - Teste Valida Mapa Caixa flutuante" ~: validaPotencialMapa pecaCordBlocoSolt ~=? False
+    , "Tarefa 1 - Teste Valida Mapa Pecas com a mesma coordenada" ~: validaPotencialMapa pecaCordMesmaCord ~=? False
+    , "Tarefa 1 - Teste Valida Mapa Cheio" ~: validaPotencialMapa pecaCordMapaCheio ~=? False
+    , "Tarefa 1 - Teste Valida Mapa com bloco chao, vazio e porta" ~: validaPotencialMapa pecaCordChaoVazioPorta ~=? True
+    , "Tarefa 1 - Teste Valida Mapa Vazio Definido" ~: validaPotencialMapa pecaCordVazioDef ~=? True
+    , "Tarefa 1 - Teste Valida Mapa Caminho Completo" ~: validaPotencialMapa pecaCordCaminhoUnico ~=? True
+    , "Tarefa 1 - Teste Valida Mapa Caminho Incompleto" ~: validaPotencialMapa pecaCordMultCaminhos ~=? False
     ]
