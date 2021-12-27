1b24fa93632a3bfc1d3bc6bdbccbbfb3e803c2a3
Ricardo Jesus <a100066@alunos.uminho.pt>
2021-12-27 23:24:12 +0000

    Adição do ficheiro da tarefa 6

---
+++ src/Tarefa6_2021li1g063.hs
@@ -0,0 +1,16 @@
+{- |
+Module      : Tarefa6_2021li1g063
+Description : Movimentação do personagem
+Copyright   : Ricardo Miguel Queirós de Jesus <a100066@alunos.uminho.pt>;
+            : Rui Pinto <a100659@alunos.uminho.pt>;
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
+-}
+
+module Tarefa6_2021li1g063 where 
+
+import LI12122
+
+resolveJogo :: Int -> Jogo -> Maybe [Movimento]
+resolveJogo = undefined
+
